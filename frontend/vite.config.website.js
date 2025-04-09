705c811943a216071d51a394f8edc97d7c316a3f
leyafo <leyafo@gmail.com>
2025-04-09 16:40:23 +0800

    add langding page

--- frontend/vite.config.website.js
+++ frontend/vite.config.website.js
@@ -5,14 +5,17 @@ import vituum from 'vituum';
 import handlebars from '@vituum/vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [
-      tailwindcss(),
-      vituum(),
-      handlebars({
-          root: './src',
-      }),
-  ],
-  build: {
-    outDir: './website', 
-  },
+    plugins: [
+        tailwindcss(),
+        vituum(),
+        handlebars({
+            root: './src',
+        }),
+    ],
+    build: {
+        outDir: './website', 
+        rollupOptions: {
+            input: ['./src/pages/**/*.hbs']
+        }
+    },
 });
