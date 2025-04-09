880e0734905953163d5566adfd5993e9eea565ef
leyafo <leyafo@gmail.com>
2025-04-09 16:34:07 +0800

    add static landing page

--- frontend/src/helpers/index.js
+++
@@ -1,9 +0,0 @@
-export {
-    timeAgo,
-    unixTimeAgo,
-    unixTimeFormat,
-} from "./time.js"
-
-export{
-    PromiseQueue
-} from "./promise_queue.js"
--- frontend/vite.config.website.js
+++ frontend/vite.config.website.js
@@ -1,11 +1,18 @@
 // vite.config.website.js
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import vituum from 'vituum';
+import handlebars from '@vituum/vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  root: 'website',
+  plugins: [
+      tailwindcss(),
+      vituum(),
+      handlebars({
+          root: './src',
+      }),
+  ],
   build: {
-    outDir: '../website_dist', 
+    outDir: './website', 
   },
 });
